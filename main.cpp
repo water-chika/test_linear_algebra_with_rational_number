f3015bd3fa7c0549e3f812522ff38df17beed521
Water Chika <water_chika@outlook.com>
2024-10-10 13:23:11 +0800

    Clean unused code and workaround compiler error.

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 #include <rational_number.hpp>
 
 int main() {
-	water::rational_number::rational_number<int32_t, uint32_t>{0} == int32_t(0);
-	linear_algebra_test::set_number<water::rational_number::rational_number<int64_t, uint64_t>>::set_index<uint8_t>::run();
+	linear_algebra_test::set_number<water::rational_number::rational_number<int64_t, int64_t>>::set_index<uint8_t>::run();
 	return 0;
 }
